75a90a049b1b7b95b1f91ada0bd6d0872f68d70f
Alex Hlotov <alexhlotov67@gmail.com>
2021-09-20 17:19:04 +0300

    Updates

---
+++ precache-manifest.5fa2854c8799533d2570d1b39fe41644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5d1ce1e3ed9a02e67ce90432e46d8f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4e2de1d3169ea5b301d",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "b0ad0d8084ab4a4bd800",
+    "url": "./static/js/2.b5d83665.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.b5d83665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e2de1d3169ea5b301d",
+    "url": "./static/js/main.f5614dce.chunk.js"
+  },
+  {
+    "revision": "12bee12ab4b288faf1d5",
+    "url": "./static/js/runtime-main.108729f7.js"
+  }
+]);
